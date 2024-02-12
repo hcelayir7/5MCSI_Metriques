dfc90f3762f16e5c81f720b6054645684ca55cd9
hcelayir7 <159788151+hcelayir7@users.noreply.github.com>
2024-02-12 18:20:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ def formulairedecontact():
 
 @app.route('/paris/')
 def meteo():
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
     results = []
